db3e8db252d2d0989ec762be78f32af0fe94848d
josue-medina <josueisraelmp@gmail.com>
2021-11-18 20:45:08 -0600

    version actualizada

--- app/src/main/java/com/example/firebasebasics/MainActivity.kt
+++ app/src/main/java/com/example/firebasebasics/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.firebasebasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.auth.User
@@ -15,10 +18,39 @@ class MainActivity : AppCompatActivity() {
 
         val myDB = FirebaseDatabase.getInstance()
         database = myDB.reference
+
+        //writeNewUser("100","JOSUE","josue@correo.com")
         //database.setValue("hola mundo")
         //database.setValue("esta es una prueba de base de datod")
 
+        val etId = findViewById<EditText>(R.id.etUserId)
+        val etNombre = findViewById<EditText>(R.id.etUserName)
+        val etCorreo = findViewById<EditText>(R.id.etUserEmail)
+        val btnSend = findViewById<Button>(R.id.btnSet)
+        btnSend.setOnClickListener {
+            writeNewUser(etId.text.toString(), etNombre.text.toString(), etCorreo.text.toString())
+            //se limpia las casillas
+            etId.text.clear()
+            etNombre.text.clear()
+            etCorreo.text.clear()
+        }
+        //getUser()
+
     }
+
+
+        //
+        fun borrar(userId: String){
+            database.child("usuarios").child(userId).removeValue()
+        }
+
+        fun getUser(userId: String){
+            database.child("usuarios").child(userId).get().addOnSuccessListener { record ->
+                Log.d("valores_de_ejemplo","got value ${record.value}")
+            }  //.addOnFailureListener { record -> Log.d("valores_de_ejemplo", "ERROR: ${record}") }
+        }
+
+
         fun writeNewUser(userId: String, name: String, email: String){
             val user = User(name,email)
 
